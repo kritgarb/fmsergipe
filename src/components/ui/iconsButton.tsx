467a2a5ab8871f2df88c040408e27e784cf91f90
Benjamin Vieira <92138212+kritgarb@users.noreply.github.com>
2024-11-18 08:26:26 -0300

    fix: outdoor, menu

---
+++ src/components/ui/iconsButton.tsx
@@ -0,0 +1,37 @@
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
+
+const IconsButton = () => (
+  <div className="flex gap-4">
+    <a
+      href="https://www.instagram.com/fmsergipe/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center gap-2 text-white rounded-md text-xl bg-[#FF060B] hover:bg-[#D00406] hover:shadow-lg transition-all duration-300 ease-in-out px-6 py-2"
+    >
+      <FaInstagram size={24} />
+      Instagram
+    </a>
+
+    <a
+      href="https://www.facebook.com/fmsergipe"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center gap-2 text-white rounded-md text-xl bg-[#FF060B] hover:bg-[#D00406] hover:shadow-lg transition-all duration-300 ease-in-out px-6 py-2"
+    >
+      <FaFacebookF size={24} />
+      Facebook
+    </a>
+
+    <a
+      href="https://twitter.com/fmsergipe"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center gap-2 text-white rounded-md text-xl bg-[#FF060B] hover:bg-[#D00406] hover:shadow-lg transition-all duration-300 ease-in-out px-6 py-2"
+    >
+      <FaTwitter size={24} />
+      Twitter
+    </a>
+  </div>
+);
+
+export default IconsButton;
--- src/sections/outdoor/index.tsx
+++ src/sections/outdoor/index.tsx
@@ -1,16 +1,15 @@
 import ICON from '@/images/Zé Bocao - sign-2.png';
 import React from 'react';
+import IconsButton from '@/components/ui/iconsButton';
 
 const styles = {
   title: 'text-6xl md:text-8xl text-black',
   descr: 'text-xl md:text-3xl w-full max-w-[600px] text-black',
   frases: 'text-left flex flex-col z-10 text-primary max-w-[700px] w-[70%]',
   icon: 'blur-lg max-w-[520px] lg:blur-none absolute right-[18vw] h-auto drop-shadow-xl transition-transform duration-300 hover:scale-110',
-
   container: 'shrink w-[80%] flex flex-row items-center max-w-[1100px]',
-  mainDiv:
-  'min-h-[700px] bg-gradient-to-r from-cor3 to-cor2 flex place-content-center p-30',
-
+  mainDiv: 'min-h-[700px] bg-gradient-to-r from-cor3 to-cor2 flex place-content-center p-30',
+  buttonContainer: 'mt-6 flex gap-4', 
 };
 
 const Outdoor = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
@@ -20,8 +19,11 @@ const Outdoor = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
         <div className={styles.frases}>
           <span className={styles.title}>Conecte-se ao som do sucesso</span>
           <span className={styles.descr}>
-          Leve sua paixão pelo rádio a um novo nível
+            Leve sua paixão pelo rádio a um novo nível
           </span>
+          <div className={styles.buttonContainer}>
+            <IconsButton />
+          </div>
         </div>
         <img className={styles.icon} alt="CFL" src={ICON}></img>
       </div>
