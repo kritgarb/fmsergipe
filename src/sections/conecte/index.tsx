920a415d254f026ac59a04b780109730dd675b8f
diego gomes de santana <diego.bakugan@hotmail.com>
2024-11-18 09:04:41 -0300

    fiz: responsividade botoes outdoor

--- src/sections/conecte/index.tsx
+++ src/sections/conecte/index.tsx
@@ -9,7 +9,7 @@ const styles = {
   mainDiv: 'bg-cor3 w-full flex place-content-center',
   case2: 'flex flex-row flex-wrap place-content-center items-center',
   img: 'w-[90%] max-w-[90%] sm:max-w-[520px] shrink mx-auto h-auto ',
-  button: 'mt-5 t-5 text-white rounded-[5px] text-xl h-12 w-40 bg-[#FF060B] hover:bg-[#6C0000] flex items-center justify-center mx-auto',
+  button: 'mt-5 flex items-center justify-center mx-auto gap-2 text-white rounded-md text-xl bg-[#FF060B] hover:bg-[#D00406] hover:shadow-lg transition-all duration-300 ease-in-out px-6 py-2',
   textArea:
     'm-5 md:m-20 text-left text-xl sm:text-2xl w-full max-w-[90%] md:max-w-80 text-black',
 };
@@ -24,13 +24,14 @@ const Conecte = React.forwardRef<HTMLDivElement, {}>((props, ref) => (
         </Atropos>
         <p className={styles.textArea}>
           Conecte-se à música, notícias e emoções que fazem seu dia melhor. Sintonize agora e faça parte da nossa sintonia!
-          <button
-            type="submit"
-            className={styles.button}
-            onClick={() => window.location.href = 'http://www.fmsergipe.com.br/radio-ao-vivo'}
+          <a
+            href="http://www.fmsergipe.com.br/radio-ao-vivo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-5 flex items-center justify-center gap-2 text-white rounded-md text-xl bg-[#FF060B] hover:bg-[#D00406] hover:shadow-lg transition-all duration-300 ease-in-out px-6 py-2"
           >
             <span>Escute a Rádio</span>
-          </button>
+          </a>
         </p>
 
       </div>
