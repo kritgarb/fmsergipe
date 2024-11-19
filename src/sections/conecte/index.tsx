e0090f7b6b90cb2f1b6c7a73394a7e2153cc27b9
diego gomes de santana <diego.bakugan@hotmail.com>
2024-11-19 11:09:23 -0300

    ajustando merge

--- src/sections/conecte/index.tsx
+++ src/sections/conecte/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import CASE2 from '../../images/FM_SERGIPE_COR_HORIZONTAL.png';
 import Atropos from 'atropos/react';
 import { Button } from '@mui/material';
@@ -13,8 +14,8 @@ const styles = {
     'm-5 md:m-20 text-left text-xl sm:text-2xl w-full max-w-[90%] md:max-w-80 text-black',
   modal: 'fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center',
   modalContent: 'bg-white p-6 rounded-lg w-[80%] max-w-[500px]',
-  closeButton: 'mt-4 text-white bg-[#FF060B] hover:bg-[#D00406] px-4 py-2 rounded',
   audioPlayer: 'w-full',
+  closeButton: 'mt-4 text-white bg-[#FF060B] hover:bg-[#D00406] px-4 py-2 rounded'
 };
 
 const Conecte = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
@@ -57,7 +58,7 @@ const Conecte = React.forwardRef<HTMLDivElement, {}>((props, ref) => {
             <div className={styles.modalContent}>
               <h3 className="text-center">Reproduzindo Rádio</h3>
               <audio controls autoPlay>
-                <source src="http://www.fmsergipe.com.br/radio-ao-vivo" type="audio/mpeg" />
+                <source src="https://08.stmip.net:8012/;" type="audio/mpeg" />
                 Seu navegador não suporta o player de áudio.
               </audio>
               <div className="text-center">
