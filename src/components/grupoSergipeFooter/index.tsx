bb28d6a414dbe4a827c12e4fb22bddd95b157be3
Lívio Gabriel <liviogabriel6@gmail.com>
2024-12-16 11:58:10 -0300

    fix: Ajustes no Footer

--- src/components/grupoSergipeFooter/index.tsx
+++ src/components/grupoSergipeFooter/index.tsx
@@ -6,15 +6,79 @@ import G1_GE from '@/images/g1ge_roxo-claro.png';
 import SEVEN_OFICINA from '@/images/sevenOficina_roxo-claro.png';
 import SEVEN_EVENTOS from '@/images/sevenEventos_roxo-claro.png';
 import GRUPO_SERGIPE_COMPLETO from '@/images/Grupo Sergipe - BLACK.png';
+import grupoSergipe from '@/images/logos-separadas/logo-grupoSergipe.png';
+import separadorGrupoSergipe from '@/images/logos-separadas/separadorGrupoSergipe.png';
+import cfl from '@/images/logos-separadas/logo-cfl.png';
+import fmSergipe from '@/images/logos-separadas/logo-FMSergipe.png';
+import g1SE from '@/images/logos-separadas/logo-g1se.png';
+import geSE from '@/images/logos-separadas/logo-gese.png';
+import globo from '@/images/logos-separadas/logo-globo.png';
+import se7enCriativa from '@/images/logos-separadas/logo-se7enCriativa.png';
+import se7enEventos from '@/images/logos-separadas/logo-se7enEventos.png';
+import tvSergipe from '@/images/logos-separadas/logo-tvsergipe.png';
 
 const imgStyle = 'w-auto max-h-[18px] sm:max-h-[30px]';
 
 const GrupoSergipeFooter = ({ style }: { style?: string }) => {
   return (
-    <div>
-      <img  src={GRUPO_SERGIPE_COMPLETO}/>
+    <div className="flex flex-row flex-wrap gap-[40px] justify-center items-center">
+      <img
+        src={grupoSergipe}
+        alt="Grupo Sergipe"
+        className="w-[30%] sm:w-[30%] md:w-[25%] lg:w-[20%] flex-grow transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("#", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={globo}
+        alt="Globo"
+        className="w-[10%] sm:w-[10%] md:w-[10%] lg:w-[5%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("https://www.globo.com/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={tvSergipe}
+        alt="TV Sergipe"
+        className="w-[20%] sm:w-[20%] md:w-[15%] lg:w-[12%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("https://redeglobo.globo.com/se/tvsergipe/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={g1SE}
+        alt="G1 SE"
+        className="w-[5%] sm:w-[5%] md:w-[8%] lg:w-[3%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("https://g1.globo.com/se/sergipe/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={geSE}
+        alt="GE SE"
+        className="w-[5%] sm:w-[5%] md:w-[8%] lg:w-[3%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("https://ge.globo.com/se/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={se7enCriativa}
+        alt="Se7en Criativa"
+        className="w-[15%] sm:w-[15%] md:w-[15%] lg:w-[8%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("http://sevenoficinacriativa.com.br/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={cfl}
+        alt="CFL"
+        className="w-[15%] sm:w-[15%] md:w-[15%] lg:w-[8%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("https://cfltecnologia.com.br/", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
+      <img
+        src={se7enEventos}
+        alt="Se7en Eventos"
+        className="w-[15%] sm:w-[15%] md:w-[15%] lg:w-[8%] transition-all duration-300 hover:scale-110"
+        onClick={() => window.open("#", "_blank")}
+        style={{ cursor: 'pointer' }}
+      />
     </div>
   );
 };
-
 export default GrupoSergipeFooter;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,105 +1,102 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ['class'],
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{ts,tsx}',
-    './sections/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-  ],
-  theme: {
-    container: {
-      center: true,
-      screens: {
-        sm: '280px',
-        md: '768px',
-        '2xl': '1400px',
-      },
-    },
-    extend: {
-      colors: {
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'var(--primary-color)',       
-        },
-        fundo: {
-          DEFAULT: 'var(--fundo-color)', 
-        },
-        cor1: {
-          DEFAULT: 'var(--cor1)',
-        },
-        cor2: {
-          DEFAULT: 'var(--cor2)',
-        },
-        cor3: {
-          DEFAULT: 'var(--cor3)',
-         
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary-color)',
-          light: 'var(--secondary-color-foreground)',
-        },
-        third: {
-          DEFAULT: 'var(--third-color)',
-        },
-        babyBlue: {
-          DEFAULT: 'var(--baby-blue)',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-      },
-      borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 4px)`,
-        sm: 'calc(var(--radius) - 8px)',
-      },
-      fontFamily: {
-        sans: ['var(--font)'],
-      },
-      fontSize: {
-        bigClamp: 'clamp(70px, 6.5vw, 130px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+export const darkMode = ['class'];
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+  './pages/**/*.{ts,tsx}',
+  './sections/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+];
+export const theme = {
+  container: {
+    center: true,
+    screens: {
+      sm: '280px',
+      md: '768px',
+      '2xl': '1400px',
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  extend: {
+    colors: {
+      border: 'hsl(var(--border))',
+      input: 'hsl(var(--input))',
+      ring: 'hsl(var(--ring))',
+      background: 'hsl(var(--background))',
+      foreground: 'hsl(var(--foreground))',
+      primary: {
+        DEFAULT: 'var(--primary-color)',
+      },
+      fundo: {
+        DEFAULT: 'var(--fundo-color)',
+      },
+      cor1: {
+        DEFAULT: 'var(--cor1)',
+      },
+      cor2: {
+        DEFAULT: 'var(--cor2)',
+      },
+      cor3: {
+        DEFAULT: 'var(--cor3)',
+      },
+      secondary: {
+        DEFAULT: 'var(--secondary-color)',
+        light: 'var(--secondary-color-foreground)',
+      },
+      third: {
+        DEFAULT: 'var(--third-color)',
+      },
+      babyBlue: {
+        DEFAULT: 'var(--baby-blue)',
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive))',
+        foreground: 'hsl(var(--destructive-foreground))',
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted))',
+        foreground: 'hsl(var(--muted-foreground))',
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent))',
+        foreground: 'hsl(var(--accent-foreground))',
+      },
+      popover: {
+        DEFAULT: 'hsl(var(--popover))',
+        foreground: 'hsl(var(--popover-foreground))',
+      },
+      card: {
+        DEFAULT: 'hsl(var(--card))',
+        foreground: 'hsl(var(--card-foreground))',
+      },
+    },
+    borderRadius: {
+      lg: `var(--radius)`,
+      md: `calc(var(--radius) - 4px)`,
+      sm: 'calc(var(--radius) - 8px)',
+    },
+    fontFamily: {
+      sans: ['var(--font)'],
+    },
+    fontSize: {
+      bigClamp: 'clamp(70px, 6.5vw, 130px)',
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+    },
+  },
 };
+export const plugins = [require('tailwindcss-animate')];
